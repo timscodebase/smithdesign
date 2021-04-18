04bf5dad2e26bd6f744d39f3290631dcb318f07b
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2021-04-18 12:23:28 -0700

    hi

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -5,12 +5,8 @@ const netlify = require("@sveltejs/adapter-netlify");
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   kit: {
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
     adapter: netlify(),
 
-    // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
 
     vite: {
