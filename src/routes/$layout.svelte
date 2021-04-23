49f89c0cbfd23557d2faa42e0312f796e5743b73
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2021-04-22 22:09:58 -0700

    WCV

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -3,6 +3,18 @@
 	import '../app.css';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import {getCLS, getFID, getLCP} from 'web-vitals';
+
+	function sendToAnalytics(metric) {
+		const body = JSON.stringify(metric);
+		// Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+		(navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+				fetch('/analytics', {body, method: 'POST', keepalive: true});
+	}
+
+	getCLS(sendToAnalytics);
+	getFID(sendToAnalytics);
+	getLCP(sendToAnalytics);
 
 	let currentPage = $page;
 
